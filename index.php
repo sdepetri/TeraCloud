d2d25bad67c18733afcfd381223ce74757e07fb1
Silvio Depetri <85438578+sdepetri@users.noreply.github.com>
2024-10-09 04:12:24 -0300

    Calc server number index.php
    change original code
    <?php
    $instance_id = gethostname();
    echo "Hola mundo!! Servido desde la instancia: " . $instance_id;
    ?>

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 $instance_id = gethostname();
-echo "Hola mundo!! Servido desde la instancia: " . $instance_id;
+// Convertir el ID de la instancia en un número
+$numeric_id = crc32($instance_id);
+// Realizar el cálculo para obtener un número entre 1 y 3
+$result = ($numeric_id % 3) + 1;
+
+echo "Hola mundo!! Servido desde la instancia: " . $instance_id . "\n";
+echo "Server # " . $result;
 ?>
+
+
+
+
