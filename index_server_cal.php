18c80fbe67b8f4bf654590103fc0e9c3d07269f5
Silvio Depetri <85438578+sdepetri@users.noreply.github.com>
2024-10-09 16:54:04 -0300

    Create index_server_cal.php
    index alternativo con calculo de servidor basado en ID de instancia

---
+++ index_server_cal.php
@@ -0,0 +1,15 @@
+<?php
+// Inicializamos la semilla aleatoria con el tiempo actual
+srand(time());
+$instance_id = gethostname();
+// Convertir el ID de la instancia en un número
+$numeric_id = crc32($instance_id);
+// Realizar el cálculo base para obtener un número entre 1 y 3
+$base_result = ($numeric_id % 3) + 1;
+// Generar un número aleatorio pequeño
+$random_factor = rand(0, 10);
+// Combinar el resultado base con el número aleatorio
+$result = ($base_result + $random_factor) % 3 + 1;
+echo "Hola mundo!! Servido desde la instancia: " . $instance_id . "\n";
+echo "Servidor #: " . $result . "\n";
+?>
